9400062a88f4767720aa719a6792422f33e15a21
rodinux <rodolphe.robles@sfr.fr>
2017-01-25 05:50:57 +0100

    try to see if the production mode is avalaible

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+#require_relative 'boot'
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
@@ -10,6 +10,7 @@ module PlanningsCinema
   class Application < Rails::Application
   	config.autoload_paths += %W(#{config.root}/lib)
   	config.eager_load_paths += %W(#{config.root}/lib)
+    config.assets.paths << Rails.root.join('vendor', 'assets', "*")
     config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js','pdf.css', 'pdf.js', 'scroll.js']
   	config.time_zone = 'Paris'
   	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]

e625aa7822eeec4a329cf86a2528e89a66ed4d9b
rodinux <rodolphe.robles@sfr.fr>
2017-01-25 02:08:32 +0100

    error of the database name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170124155905) do
-  
+
 enable_extension "plpgsql"
 
   create_table "classifications", force: :cascade do |t|

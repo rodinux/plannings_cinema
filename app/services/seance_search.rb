645fb8983128c75f1a32482ebf48a9baedf43108
rodinux <rodolphe.robles@sfr.fr>
2017-05-26 00:46:49 +0200

    add a description help for disponibilités

--- app/services/seance_search.rb
+++ app/services/seance_search.rb
@@ -3,8 +3,8 @@ class SeanceSearch
 
 	def initialize(params)
 		params ||= {}
-		@date_from = parsed_date(params[:date_from], 7.days.ago.to_date.to_s)
-		@date_to = parsed_date(params[:date_to], Date.today.to_s)
+		@date_from = parsed_date(params[:date_from], 7.days.ago)
+		@date_to = parsed_date(params[:date_to], Date.today)
 	end
 
 	def scope

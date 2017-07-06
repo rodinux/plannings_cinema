c39465ede08d4951ed1e3cd8f59c778da1b34f33
rodinux <rodolphe.robles@sfr.fr>
2017-07-06 14:38:49 +0200

    come back to last code

--- app/models/disponibilite.rb
+++ app/models/disponibilite.rb
@@ -1,14 +1,3 @@
 class Disponibilite < ApplicationRecord
-	before_create :setup_default_value_for_new_disponibilites
-
-	private
-
-	def setup_default_value_for_new_disponibilites
-	    if self.start_time.blank?
-	        self.start_time = Date.today
-	    end
-	    if self.end_time.blank?
-	        self.end_time = Date.today
-	    end
-	end
+	
 end

16b99327c59387c134361dd6852598de9e3d53ca
rodinux <rodolphe.robles@sfr.fr>
2018-05-04 15:14:23 +0200

    allow iframe for site calendrierecranvillage.deploiement.ovh test

--- app/controllers/iframe_controller.rb
+++ app/controllers/iframe_controller.rb
@@ -1,6 +1,6 @@
 class IframeController < ApplicationController
   def iframe_action
     response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://programme.ecranvillage.deploiement.ovh/"
-    render layout: false
+    # render
   end
 end

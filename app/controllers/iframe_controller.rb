a1be3e7331b6e4ce2e178d74aa51e5a4f30feb3c
rodinux <rodolphe.robles@sfr.fr>
2018-05-04 16:29:11 +0200

    allow iframe for all sites test

--- app/controllers/iframe_controller.rb
+++ app/controllers/iframe_controller.rb
@@ -2,5 +2,5 @@ class IframeController < ApplicationController
   #def iframe_action
     #response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://programme.ecranvillage.deploiement.ovh/"
     # render
-  end
+  #end
 end

0e787da967fd530dd6710c88a71529349184970b
rodinux <rodolphe.robles@sfr.fr>
2018-05-04 15:04:04 +0200

    allow iframe for site calendrierecranvillage.deploiement.ovh test

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module PlanningsCinema
     config.beginning_of_week = :wednesday
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

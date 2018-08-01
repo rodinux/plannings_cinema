402944fa623ba1ab6c20b441b84908f85980acbf
Robles Rodolphe <rodolphe.robles@sfr.fr>
2018-08-01 21:01:45 +0200

    add gem railroady and make modelisation within

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'railroady'
   # Use sqlite3 as the database for Active Record
   #gem 'sqlite3'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
--- app/controllers/classifications_controller.rb
+++ app/controllers/classifications_controller.rb
@@ -65,7 +65,7 @@ class ClassificationsController < ApplicationController
   end
 
   private
-  
+
     # Use callbacks to share common setup or constraints between actions.
     def set_classification
       @classification = Classification.find(params[:id])

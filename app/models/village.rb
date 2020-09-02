18508ba05fb1aaba8b14e2bf960b2817a5e45c60
Robles Rodolphe <rodolphe.robles@sfr.fr>
2020-09-02 22:12:44 +0200

    upgrade some changes avalaible in the production app

--- app/models/village.rb
+++ app/models/village.rb
@@ -1,8 +1,26 @@
 class Village < ApplicationRecord
-		has_many :seances, :dependent => :destroy, :inverse_of => :village
-		accepts_nested_attributes_for :seances, :allow_destroy => true
-		has_many :films, :through => :seances
-		validates :id, :uniqueness => true, :case_sensitive => false
-	    validates :commune, :presence => true, :case_sensitive => false
-	    validates :salle, :presence => true, :case_sensitive => false
+  has_many :seances, :dependent => :destroy, :inverse_of => :village
+  accepts_nested_attributes_for :seances, :allow_destroy => true
+  has_many :films, :through => :seances
+  validates :id, :uniqueness => true, :case_sensitive => false
+  validates :commune, :presence => true, :case_sensitive => false
+  validates :salle, :presence => true, :case_sensitive => false
+  validates :import_id, :uniqueness => true, :case_sensitive => false
+
+  before_create :setup_default_value_for_new_villages
+  #throw(:abort)
+
+  private
+
+  def setup_default_value_for_new_villages
+      village_import_last = Village.order(:import_id).last
+      if self.import_id.blank?
+        self.import_id = village_import_last.import_id + 1
+      end
+  end
+
 end
+
+
+
+
